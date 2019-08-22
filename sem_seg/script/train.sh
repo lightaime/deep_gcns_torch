1ae220815a132e670c85fd4ae64494160c1abb01
guochengqian <gordonqian2017@gmail.com>
2019-08-22 16:08:00 +0300

    ckpt

--- sem_seg/script/train.sh
+++ sem_seg/script/train.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 conda activate deepgcn
-python -u sem_seg/train.py  --multi_gpus --train
-#--train_path data/3D/S3DIS
-#--batch_size 16
+python -u sem_seg/train.py  --multi_gpus --train --train_path data/3D/S3DIS --batch_size 16
 echo "===> training loaded Done..."
 
