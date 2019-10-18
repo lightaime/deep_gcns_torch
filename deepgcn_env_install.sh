ebdd13f9f60d4b9ad78d3aaa35069e43a39fe4dc
guochengqian <gordonqian2017@gmail.com>
2019-10-18 18:31:02 +0300

    environment details

--- deepgcn_env_install.sh
+++ deepgcn_env_install.sh
@@ -6,23 +6,18 @@
 # wget https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh
 # bash Anaconda3-2019.07-Linux-x86_64.sh
 
-# module load, commet if using local machine
-#module purge
-#module load gcc
-#module load cuda/10.1.105
-#
-# make sure your annaconda3 is added to bashrc
+
 source ~/.bashrc
+
+# make sure system cuda version is the same with pytorch cuda
+# follow the instruction of Pyotrch Geometric: https://pytorch-geometric.readthedocs.io/en/latest/notes/installation.html
+export PATH=/usr/local/cuda-10.0/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:$LD_LIBRARY_PATH
 
 conda create -n deepgcn
 conda activate deepgcn
-conda install -y pytorch torchvision cudatoolkit=10.0 tensorflow python=3.7 -c pytorch
-
-# uncomment if you use your own cuda (not by module load) or uncomment cuda path is already in ~/.bashrc
-# export PATH=/usr/local/cuda/bin:$PATH
-# export CPATH=/usr/local/cuda/include:$CPATH
-# export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
-# export DYLD_LIBRARY_PATH=/usr/local/cuda/lib:$DYLD_LIBRARY_PATH
+# make sure pytorch version >=1.2.0
+conda install -y pytorch=1.2.0 torchvision cudatoolkit=10.0 tensorflow python=3.7 -c pytorch
 
 # command to install pytorch geometric
 pip install --verbose --no-cache-dir torch-scatter
@@ -34,4 +29,3 @@ pip install --upgrade tensorflow-graphics
 # install useful modules
 pip install requests # sometimes pytorch geometric forget to install it, but it is in need
 pip install tqdm
-
