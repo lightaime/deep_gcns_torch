75276dafb1eb79ab6aa3052030a2f6e034a83089
guochengqian <gordonqian2017@gmail.com>
2020-05-21 18:03:51 +0300

    add description in torch_vertex. remove useless part.

--- gcn_lib/dense/torch_vertex.py
+++ gcn_lib/dense/torch_vertex.py
@@ -73,7 +73,7 @@ class DynConv2d(GraphConv2d):
     
 class PlainDynBlock2d(nn.Module):
     """
-    Residual Dynamic graph convolution block
+    Plain Dynamic graph convolution block
     """
     def __init__(self, in_channels, kernel_size=9, dilation=1, conv='edge', act='relu', norm=None,
                  bias=True,  stochastic=False, epsilon=0.0, knn='matrix'):
@@ -113,65 +113,3 @@ class DenseDynBlock2d(nn.Module):
     def forward(self, x):
         dense = self.body(x)
         return torch.cat((x, dense), 1)
-
-
-class GraphPooling(nn.Module):
-    """
-    Dense Dynamic graph pooling block
-    """
-    def __init__(self, in_channels, ratio=0.5, conv='edge', **kwargs):
-        super(GraphPooling, self).__init__()
-        self.gnn = DynConv2d(in_channels, 1, conv=conv, **kwargs)
-        self.ratio = ratio
-
-    def forward(self, x):
-        """"""
-        score = torch.tanh(self.gnn(x))
-        _, indices = score.topk(int(x.shape[2]*self.ratio), 2)
-        return torch.gather(x, 2, indices.repeat(1, x.shape[1], 1, 1))
-
-
-class VLADPool(torch.nn.Module):
-    def __init__(self, in_channels, num_clusters=64, alpha=100.0):
-        super(VLADPool, self).__init__()
-        self.in_channels = in_channels
-        self.num_clusters = num_clusters
-        self.alpha = alpha
-
-        self.lin = nn.Linear(in_channels, self.num_clusters, bias=True)
-
-        self.centroids = nn.Parameter(torch.rand(self.num_clusters, in_channels))
-        self._init_params()
-
-    def _init_params(self):
-        self.lin.weight = nn.Parameter((2.0 * self.alpha * self.centroids))
-        self.lin.bias = nn.Parameter(- self.alpha * self.centroids.norm(dim=1))
-
-    def forward(self, x, norm_intra=False, norm_L2=False):
-        B, C, N, _ = x.shape
-        x = x.squeeze().transpose(1, 2)  # B, N, C
-        K = self.num_clusters
-        soft_assign = self.lin(x)  # soft_assign of size (B, N, K)
-        soft_assign = F.softmax(soft_assign, dim=1).unsqueeze(1)  # soft_assign of size (B, N, K)
-        soft_assign = soft_assign.expand(-1, C, -1, -1)  # soft_assign of size (B, C, N, K)
-
-        # input x of size (NxC)
-        xS = x.transpose(1, 2).unsqueeze(-1).expand(-1, -1, -1, K)  # xS of size (B, C, N, K)
-        cS = self.centroids.unsqueeze(0).unsqueeze(0).expand(B, N, -1, -1).transpose(2, 3)  # cS of size (B, C, N, K)
-
-        residual = (xS - cS)  # residual of size (B, C, N, K)
-        residual = residual * soft_assign  # vlad of size (B, C, N, K)
-
-        vlad = torch.sum(residual, dim=2, keepdim=True)  # (B, C, K)
-
-        if (norm_intra):
-            vlad = F.normalize(vlad, p=2, dim=1)  # intra-normalization
-            # print("i-norm vlad", vlad.shape)
-        if (norm_L2):
-            vlad = vlad.view(-1, K * C)  # flatten
-            vlad = F.normalize(vlad, p=2, dim=1)  # L2 normalize
-
-        # return vlad.view(B, -1, 1, 1)
-        return vlad
-
-
