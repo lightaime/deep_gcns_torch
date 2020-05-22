6b6099402d93b9f263fa6ced183afa1208447fe9
guochengqian <gordonqian2017@gmail.com>
2020-05-23 02:52:26 +0300

    updated visualization code. add comments

--- examples/part_sem_seg/visualize.py
+++ examples/part_sem_seg/visualize.py
@@ -12,10 +12,23 @@ category_names = ['Bag', 'Bed', 'Bottle', 'Bowl', 'Chair', 'Clock', 'Dishwasher'
         'Faucet', 'Hat', 'Keyboard', 'Knife', 'Lamp', 'Laptop', 'Microwave', 'Mug', 'Refrigerator', 'Scissors',  # 10-19
         'StorageFurniture', 'Table', 'TrashCan', 'Vase']  # 20-23
 
-parser = argparse.ArgumentParser(description='PyTorch implementation of Deep GCN')
-parser.add_argument('--category', type=int, default=1)
+parser = argparse.ArgumentParser(description='Qualitative comparision of ResGCN '
+                                             'against PlainGCN on PartNet segmentation')
+
+# dir_path set to the location of the result folder.
+# result folder should have such structure:
+# result
+# ├── plain  # result folder for PlainGCN
+#      ├── Bed # the obj director of category Bed
+#            ├── Bed_0_pred.obj
+# ├── res  # result folder for ResGCN
+#      ├── Bed # the obj director of category Bed
+#            ├── Bed_0_pred.obj
+
+parser.add_argument('--category', type=int, default=4)
 parser.add_argument('--obj_no', default=0, type=int, help='NO. of which obj in a given category to visualize')
-parser.add_argument('--folders', default='res,plain', type=str,
+parser.add_argument('--dir_path', default='../result', type=str, help='path to the result')
+parser.add_argument('--folders', default='plain,res', type=str,
                     help='use "," to separate different folders, eg. "res,plain"')
 args = parser.parse_args()
 
@@ -23,16 +36,16 @@ category = category_names[args.category]
 obj_no = args.obj_no
 folders = list(map(lambda x: x.strip(), args.folders.split(',')))
 
-dir_path = osp.join(os.path.dirname(os.path.abspath(__file__)), 'result')
-folder_paths = list(map(lambda x: osp.join(dir_path, x, category), folders))
+folder_paths = list(map(lambda x: osp.join(args.dir_path, x, category), folders))
 
 file_name_pred = '_'.join([category, str(obj_no), 'pred.obj'])
 file_name_gt = '_'.join([category, str(obj_no), 'gt.obj'])
 
+# show Ground Truth, PlainGCN, ResGCN
 visualize_part_seg(file_name_pred,
                    file_name_gt,
                    folder_paths,
                    limit=-1,
-                   text=['Ground Truth', 'ResGCN-28'],
+                   text=['Ground Truth', 'PlainGCN-28', 'ResGCN-28'],
                    interactive=True,
                    orientation='horizontal')
