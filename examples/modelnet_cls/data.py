915d03ffe05e656b540abefab8b5e94dd9bf2db1
guochengqian <gordonqian2017@gmail.com>
2020-05-27 19:08:14 +0300

    datadir -> data_dir (keep naming the same)

--- examples/modelnet_cls/data.py
+++ examples/modelnet_cls/data.py
@@ -6,22 +6,22 @@ import numpy as np
 from torch.utils.data import Dataset
 
 
-def download(datadir):
-    if not os.path.exists(datadir):
-        os.makedirs(datadir)
-    if not os.path.exists(os.path.join(datadir, 'modelnet40_ply_hdf5_2048')):
+def download(data_dir):
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+    if not os.path.exists(os.path.join(data_dir, 'modelnet40_ply_hdf5_2048')):
         www = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
         zipfile = os.path.basename(www)
         os.system('wget %s; unzip %s' % (www, zipfile))
-        os.system('mv %s %s' % (zipfile[:-4], datadir))
+        os.system('mv %s %s' % (zipfile[:-4], data_dir))
         os.system('rm %s' % (zipfile))
 
 
-def load_data(datadir, partition):
-    download(datadir)
+def load_data(data_dir, partition):
+    download(data_dir)
     all_data = []
     all_label = []
-    for h5_name in glob.glob(os.path.join(datadir, 'modelnet40_ply_hdf5_2048', 'ply_data_%s*.h5'%partition)):
+    for h5_name in glob.glob(os.path.join(data_dir, 'modelnet40_ply_hdf5_2048', 'ply_data_%s*.h5'%partition)):
         with h5py.File(h5_name, 'r') as f:
             data = f['data'][:].astype('float32')
             label = f['label'][:].astype('int64')
@@ -50,11 +50,11 @@ class ModelNet40(Dataset):
     ModelNet40 contains 12,311 meshed CAD models from 40 categories.
 
     num_points: 1024 by default
-    datadir
+    data_dir
     paritition: train or test
     """
-    def __init__(self, num_points=1024, datadir="/data/deepgcn/modelnet40", partition='train'):
-        self.data, self.label = load_data(datadir, partition)
+    def __init__(self, num_points=1024, data_dir="/data/deepgcn/modelnet40", partition='train'):
+        self.data, self.label = load_data(data_dir, partition)
         self.num_points = num_points
         self.partition = partition
 
