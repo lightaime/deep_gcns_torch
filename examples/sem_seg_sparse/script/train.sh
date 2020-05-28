1ac5f4d526759ab16cd845e43694a8d592b8888b
Guocheng Qian <48788073+guochengqian@users.noreply.github.com>
2020-05-28 19:31:06 +0300

    Update train.sh

--- examples/sem_seg_sparse/script/train.sh
+++ examples/sem_seg_sparse/script/train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 conda activate deepgcn
-python -u examples/sem_seg_sparse/train.py  --multi_gpus --phase train --train_path /data/deepgcn/S3DIS --batch_size 16
+python -u train.py  --multi_gpus --phase train --train_path /data/deepgcn/S3DIS --batch_size 16
 echo "===> training loaded Done..."
 
